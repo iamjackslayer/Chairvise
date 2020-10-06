ef283b6921bba55b24a7f20288b4bd8833133f89
Jack Wong <jackwong3101@yahoo.com>
2020-10-06 14:50:24 +0800

    add handler for row selection

--- src/web/app/src/components/chairhub/PublicPostList.vue
+++ src/web/app/src/components/chairhub/PublicPostList.vue
@@ -92,145 +92,151 @@
   </b-container>
 </template>
 <script>
-  export default {
-    methods: {
-      onRowSelected(row) {
-        console.log(row[0].conference);
+export default {
+  methods: {
+    onRowSelected(row) {
+      // console.log(row[0].conference);
+      this.$router.push({
+        name: "section",
+        params: {
+          id: row[0].id
+        }
+      });
+    }
+  },
+  data() {
+    return {
+      perPage: 10,
+      currentPage: 1,
+      pageOptions: [10, 20, 50],
+      totalRows: 1,
+      sortBy: "created_on",
+      sortDesc: true,
+      filter: "",
+      transProps: {
+        name: "flip-list"
       },
-    },
-    data() {
-      return {
-        perPage: 10,
-        currentPage: 1,
-        pageOptions: [10, 20, 50],
-        totalRows: 1,
-        sortBy: "created_on",
-        sortDesc: true,
-        filter: "",
-        transProps: {
-          name: "flip-list",
+      fields: [
+        { key: "id", sortable: true },
+        { key: "conference" },
+        { key: "created_by" },
+        { key: "created_on", sortable: true }
+      ],
+      items: [
+        {
+          id: 1,
+          conference:
+            "ICCCIT 2020: Computer, Communications and Information Technology Conference",
+          created_by: "Dickerson",
+          created_on: "10/10/2020"
         },
-        fields: [
-          { key: "id", sortable: true },
-          { key: "conference" },
-          { key: "created_by" },
-          { key: "created_on", sortable: true },
-        ],
-        items: [
-          {
-            id: 1,
-            conference:
-              "ICCCIT 2020: Computer, Communications and Information Technology Conference",
-            created_by: "Dickerson",
-            created_on: "10/10/2020",
-          },
-          {
-            id: 2,
-            conference:
-              "ICCITIA 2020: Computer and Information Technologies, Innovations and Applications Conference",
-            created_by: "Larsen",
-            created_on: "1/10/2020",
-          },
-          {
-            id: 3,
-            conference: "ICCM 2020: Cognitive Modeling Conference",
-            created_by: "Geneva",
-            created_on: "2/10/2020",
-          },
-          {
-            id: 4,
-            conference:
-              "ICCNSHE 2020: Computer, Network, Software and Hardware Engineering Conference",
-            created_by: "Jami",
-            created_on: "3/10/2020",
-          },
-          {
-            id: 5,
-            conference:
-              "ICCCIT 2020: Computer, Communications and Information Technology Conference",
-            created_by: "Dickerson",
-            created_on: "31/09/2020",
-          },
-          {
-            id: 6,
-            conference:
-              "ICCITIA 2020: Computer and Information Technologies, Innovations and Applications Conference",
-            created_by: "Larsen",
-            created_on: "10/10/2020",
-          },
-          {
-            id: 7,
-            conference: "ICCM 2020: Cognitive Modeling Conference",
-            created_by: "Geneva",
-            created_on: "10/10/2020",
-          },
-          {
-            id: 8,
-            conference:
-              "ICCNSHE 2020: Computer, Network, Software and Hardware Engineering Conference",
-            created_by: "Jami",
-            created_on: "1/10/2020",
-          },
-          {
-            id: 9,
-            conference:
-              "ICCCIT 2020: Computer, Communications and Information Technology Conference",
-            created_by: "Dickerson",
-            created_on: "1/12/2019",
-          },
-          {
-            id: 10,
-            conference:
-              "ICCITIA 2020: Computer and Information Technologies, Innovations and Applications Conference",
-            created_by: "Larsen",
-            created_on: "10/10/2020",
-          },
-          {
-            id: 11,
-            conference: "ICCM 2020: Cognitive Modeling Conference",
-            created_by: "Geneva",
-            created_on: "10/10/2020",
-          },
-          {
-            id: 12,
-            conference:
-              "ICCNSHE 2020: Computer, Network, Software and Hardware Engineering Conference",
-            created_by: "Jami",
-            created_on: "10/10/2020",
-          },
-        ],
-      };
-    },
-    mounted() {
-      this.totalRows = this.items.length;
-    },
-  };
+        {
+          id: 2,
+          conference:
+            "ICCITIA 2020: Computer and Information Technologies, Innovations and Applications Conference",
+          created_by: "Larsen",
+          created_on: "1/10/2020"
+        },
+        {
+          id: 3,
+          conference: "ICCM 2020: Cognitive Modeling Conference",
+          created_by: "Geneva",
+          created_on: "2/10/2020"
+        },
+        {
+          id: 4,
+          conference:
+            "ICCNSHE 2020: Computer, Network, Software and Hardware Engineering Conference",
+          created_by: "Jami",
+          created_on: "3/10/2020"
+        },
+        {
+          id: 5,
+          conference:
+            "ICCCIT 2020: Computer, Communications and Information Technology Conference",
+          created_by: "Dickerson",
+          created_on: "31/09/2020"
+        },
+        {
+          id: 6,
+          conference:
+            "ICCITIA 2020: Computer and Information Technologies, Innovations and Applications Conference",
+          created_by: "Larsen",
+          created_on: "10/10/2020"
+        },
+        {
+          id: 7,
+          conference: "ICCM 2020: Cognitive Modeling Conference",
+          created_by: "Geneva",
+          created_on: "10/10/2020"
+        },
+        {
+          id: 8,
+          conference:
+            "ICCNSHE 2020: Computer, Network, Software and Hardware Engineering Conference",
+          created_by: "Jami",
+          created_on: "1/10/2020"
+        },
+        {
+          id: 9,
+          conference:
+            "ICCCIT 2020: Computer, Communications and Information Technology Conference",
+          created_by: "Dickerson",
+          created_on: "1/12/2019"
+        },
+        {
+          id: 10,
+          conference:
+            "ICCITIA 2020: Computer and Information Technologies, Innovations and Applications Conference",
+          created_by: "Larsen",
+          created_on: "10/10/2020"
+        },
+        {
+          id: 11,
+          conference: "ICCM 2020: Cognitive Modeling Conference",
+          created_by: "Geneva",
+          created_on: "10/10/2020"
+        },
+        {
+          id: 12,
+          conference:
+            "ICCNSHE 2020: Computer, Network, Software and Hardware Engineering Conference",
+          created_by: "Jami",
+          created_on: "10/10/2020"
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.totalRows = this.items.length;
+  }
+};
 </script>
 <style lang="scss" scoped>
-  .public-post-list {
-    height: 500px;
-  }
+.public-post-list {
+  height: 500px;
+}
 </style>
 <style lang="scss">
-  .public-post-list-pagination {
-    li {
-      color: aqua;
-    }
-    .page-link {
-      color: $gray-800;
-    }
+.public-post-list-pagination {
+  li {
+    color: aqua;
   }
-  .public-post-list {
-    .flip-list-move {
-      transition: all 0.5s ease-in-out;
-    }
-    .flip-list-enter {
-      transform: scale(1);
-      transition: all 0.5s ease-in-out;
-    }
-    .flip-list-leave {
-      transform: scale(0);
-      transition: all 0.5s ease-in-out;
-    }
+  .page-link {
+    color: $gray-800;
   }
+}
+.public-post-list {
+  .flip-list-move {
+    transition: all 0.5s ease-in-out;
+  }
+  .flip-list-enter {
+    transform: scale(1);
+    transition: all 0.5s ease-in-out;
+  }
+  .flip-list-leave {
+    transform: scale(0);
+    transition: all 0.5s ease-in-out;
+  }
+}
 </style>
