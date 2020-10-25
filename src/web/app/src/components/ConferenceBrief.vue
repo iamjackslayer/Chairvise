450df59b4d8bf0a62e3a88414eb0c85fd39e8af2
randytqw <randy.tan@iotamedtech.com>
2020-10-26 00:50:33 +0800

    edit home page and conference page

--- src/web/app/src/components/ConferenceBrief.vue
+++ src/web/app/src/components/ConferenceBrief.vue
@@ -1,66 +1,85 @@
 <template>
   <div class="page-container">
-    <b-breadcrumb>
-      <b-breadcrumb-item to="/conference">My Conferences</b-breadcrumb-item>
-      <b-breadcrumb-item active>Conference Details</b-breadcrumb-item>
-    </b-breadcrumb>
-    <b-card class="details-with-form">
-      <div slot="header">
-        <span>Conference Details</span>
-      </div>
-      <b-form @submit.stop.prevent>
-        <b-overlay :show="isLoading" no-wrap />
-        <b-alert v-if="isError" show variant="danger" class="mb-4">
-          <b-icon
-            class="alert-icon"
-            icon="exclamation-circle-fill"
-            variant="danger"
-          />
-          {{ apiErrorMsg }}
-        </b-alert>
-        <b-form-group label="Name" label-for="name">
-          <div v-if="!isInEditMode">{{ conferenceFormName }}</div>
-          <b-form-input
-            id="name"
-            name="name"
-            v-if="isInEditMode"
-            v-model="conferenceFormName"
-          />
-        </b-form-group>
-
-        <b-form-group label="Description" label-for="description">
-          <div v-if="!isInEditMode">
-            {{ conferenceFormDescription || "-" }}
-          </div>
-          <b-form-textarea
-            id="description"
-            name="description"
-            v-if="isInEditMode"
-            v-model="conferenceFormDescription"
-          />
-        </b-form-group>
-
-        <b-form-group label="Date" label-for="date">
-          <div v-if="!isInEditMode" id="conference-date">
-            {{ dayjs(conferenceFormDate) }}
-          </div>
-          <b-form-datepicker
-            id="date"
-            name="date"
-            v-if="isInEditMode"
-            v-model="conferenceFormDate"
-          />
-        </b-form-group>
-      </b-form>
-      <b-button
-        variant="outline-danger"
-        v-if="!isNewConference && isLogin"
-        @click="deleteConference()"
+    <b-row>
+      <b-col>
+        <b-breadcrumb>
+          <b-breadcrumb-item to="/conference">My Conferences</b-breadcrumb-item>
+          <b-breadcrumb-item active>Conference Details</b-breadcrumb-item>
+        </b-breadcrumb>
+      </b-col>
+    </b-row>
+    <b-row class="row">
+      <b-col
+        ><b-card header="Author Records"><h1>hello</h1></b-card></b-col
       >
-        <b-icon icon="trash-fill" aria-hidden="true"></b-icon>
-        Delete Conference
-      </b-button>
-    </b-card>
+      <b-col
+        ><b-card header="Submission Records"><h1>hello</h1></b-card></b-col
+      >
+      <b-col
+        ><b-card header="Review Records"><h1>hello</h1></b-card>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-card class="details-with-form">
+          <div slot="header">
+            <span>Conference Details</span>
+          </div>
+          <b-form @submit.stop.prevent>
+            <b-overlay :show="isLoading" no-wrap />
+            <b-alert v-if="isError" show variant="danger" class="mb-4">
+              <b-icon
+                class="alert-icon"
+                icon="exclamation-circle-fill"
+                variant="danger"
+              />
+              {{ apiErrorMsg }}
+            </b-alert>
+            <b-form-group label="Name" label-for="name">
+              <div v-if="!isInEditMode">{{ conferenceFormName }}</div>
+              <b-form-input
+                id="name"
+                name="name"
+                v-if="isInEditMode"
+                v-model="conferenceFormName"
+              />
+            </b-form-group>
+
+            <b-form-group label="Description" label-for="description">
+              <div v-if="!isInEditMode">
+                {{ conferenceFormDescription || "-" }}
+              </div>
+              <b-form-textarea
+                id="description"
+                name="description"
+                v-if="isInEditMode"
+                v-model="conferenceFormDescription"
+              />
+            </b-form-group>
+
+            <b-form-group label="Date" label-for="date">
+              <div v-if="!isInEditMode" id="conference-date">
+                {{ dayjs(conferenceFormDate) }}
+              </div>
+              <b-form-datepicker
+                id="date"
+                name="date"
+                v-if="isInEditMode"
+                v-model="conferenceFormDate"
+              />
+            </b-form-group>
+          </b-form>
+          <b-button
+            variant="outline-danger"
+            v-if="!isNewConference && isLogin"
+            @click="deleteConference()"
+          >
+            <b-icon icon="trash-fill" aria-hidden="true"></b-icon>
+            Delete Conference
+          </b-button>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -242,4 +261,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.row {
+  margin: 20px;
+}
+</style>
--- src/web/app/src/components/homePageDetail/BannerDetail.vue
+++ src/web/app/src/components/homePageDetail/BannerDetail.vue
@@ -1,25 +1,51 @@
 <template>
-  <div
-    class="jumbotron jumbotron-fluid"
-    style="background:linear-gradient(to bottom, #add8e6, #fafafa)"
-  >
-    <zoom-center-transition :duration="500">
-      <b-row style="text-align:center">
-        <b-col>
-          <img alt="Vue logo" src="@/assets/chair2.png" />
-        </b-col>
-        <b-col>
-          <h2>
-            ChairVisE -- The Conference Data Visualisation Management System
-          </h2>
-        </b-col>
-      </b-row>
-    </zoom-center-transition>
+  <div>
+    <div
+      class="jumbotron jumbotron-fluid"
+      style="background:linear-gradient(to bottom, #add8e6, #fafafa)"
+    >
+      <zoom-center-transition :duration="500">
+        <b-row style="text-align:center">
+          <b-col>
+            <img alt="Vue logo" src="@/assets/chair2.png" />
+          </b-col>
+          <b-col>
+            <h2>
+              ChairVisE -- The Conference Data Visualisation Management System
+            </h2>
+          </b-col>
+        </b-row>
+      </zoom-center-transition>
+    </div>
+    <div class="page">
+      <b-card
+        class="shadow p-3 mb-5 bg-white rounded"
+        v-if="isPresentationListEmpty"
+      >
+        <!-- TODO: Replace with openpeeps image -->
+      </b-card>
+      <div class="presentation-grid">
+        <b-card
+          class="presentation-card"
+          v-for="conference in conferences"
+          :key="conference.id"
+          @click="viewConference(conference.id)"
+        >
+          <h5 class="presentation-title">{{ conference.name }}</h5>
+          <div class="presentation-description">
+            {{ conference.description || "-" }}
+          </div>
+          <div class="privacy-status">
+            <b-icon icon="lock" class="mr-1" />Private
+          </div>
+        </b-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { ZoomCenterTransition } from "vue2-transitions";
+import dayjs from "dayjs";
 export default {
   name: "BannerDetail",
   data: () => ({
@@ -30,20 +56,85 @@ export default {
   },
   mounted() {
     this.loadBanner();
+    this.$store.dispatch("getConferenceList");
+    this.loadConferences();
+  },
+  computed: {
+    isLoading() {
+      return this.$store.state.conference.conferenceListStatus.isLoading;
+    },
+    conferences() {
+      return this.$store.state.conference.conferenceList;
+    }
   },
   methods: {
     loadBanner() {
       this.show = true;
+    },
+    loadConferences() {
+      this.show = true;
+    },
+    dayjs(date) {
+      return dayjs(date).format("dddd, MMMM D, YYYY");
+    },
+    viewConference(id) {
+      this.$router.push("/conference/" + id);
     }
-  },
-  components: {
-    ZoomCenterTransition
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+@supports (display: grid) {
+  .presentation-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    // TODO: Change to one column for mobile
+    grid-gap: 1.5rem;
+  }
+}
+.row {
+  margin: 20px;
+}
+.presentation-card {
+  cursor: pointer;
+}
+
+.presentation-card:hover {
+  background-color: $indigo-100;
+}
+
+.presentation-card > .card-body {
+  display: flex;
+  flex-direction: column;
+}
+.page {
+  padding: 20px;
+}
+.presentation-description {
+  font-size: 0.95rem;
+  letter-spacing: -0.025em;
+  color: $gray-600;
+  // clamp to 3 lines (restricted to webkit browsers)
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+}
+
+.privacy-status {
+  margin-top: auto;
+  font-size: 0.95rem;
+  color: $indigo-600;
+  align-self: flex-end;
+}
+
+.presentation-title {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
 h2 {
   font-family: Garamond;
 }
@@ -53,9 +144,5 @@ img {
   text-align: center;
   display: block;
   margin: 0 auto;
-}
-.el-row {
-  background: #1e9fc4;
-  padding: 32px;
 }
 </style>
