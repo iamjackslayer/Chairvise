8d67e9ac576e00478d95519d2c2b376eeba8c576
Jing Han Sem <semjinghan@gmail.com>
2020-10-27 17:54:56 +0800

    Fix mobile responsiveness for title bar

--- src/web/app/src/App.vue
+++ src/web/app/src/App.vue
@@ -3,7 +3,7 @@
     <div class="row flex-xl-nowrap">
       <SideBar />
 
-      <div class="col-12 col-lg-9 col-xl-10 py-2 py-lg-3 px-2 px-lg-5">
+      <div class="col-12 col-lg-9 col-xl-10 py-2 py-lg-3 px-3 px-lg-5">
         <b-overlay :show="isAppLoading" no-wrap />
         <div class="page-container">
           <router-view />
